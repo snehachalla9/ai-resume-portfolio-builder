acdf3d85535c5449268bacb375e6261ad705497c
snehachalla9 <snehachalla73@gmail.com>
2026-01-18 10:44:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+st.set_page_config(page_title="AI RESUME AND PORTFOLIO BUILDER")
+from pages_ui.home import show_home
+from pages_ui.resume_analyzer import show_resume_analyzer
+from pages_ui.resume_templates import show_resume_templates
+from pages_ui.build_resume import show_build_resume
+from pages_ui.portfoilo_Builder import   show_portfolio_builder
+from pages_ui.myaccount import my_account
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in=False
+if not st.session_state.logged_in:
+    st.title("AI RESUME")
+    email=st.text_input("Email")
+    password=st.text_input("password")
+    if st.button("Login"):
+        if(email=="sneha@gmail.com" and password=="sneha"):
+            st.session_state.logged_in=True
+            st.success("Login suceesfully")
+            st.rerun()
+        else:
+            st.error("Invalid email or password")
+    #st.title("AI RESUME AND PORTFOLIO")
+else:
+    menu=st.sidebar.radio(
+        "Navigation",
+        ["Home","Resume Analyzer","Resume Templates","Build My Resume","Portfolio Builder","My Account"])
+    if menu == "Home":
+        show_home()
+ 
+    elif menu=="Resume Analyzer":
+        show_resume_analyzer()
+    elif menu=="Resume Templates":
+        show_resume_templates()
+    elif menu=="Build My Resume":
+        show_build_resume()
+    elif menu=="Portfolio Builder":
+        show_portfolio_builder()
+    elif menu=="My Account":
+        my_account()
+        st.stop()
